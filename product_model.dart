090660d8454061d23a4c781842692114aca4d213
RhnFdly <151982785+RhnFdly@users.noreply.github.com>
2025-11-21 15:39:01 +0700

    Update product_model.dart

--- product_model.dart
+++ product_model.dart
@@ -1,1 +1,44 @@
+class ProductModel {
+  final String id;
+  final String name;
+  final int price;
+  final String image;
 
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+
+  // Metode untuk mengkonversi object ke Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'image': image,
+    };
+  }
+
+  // Metode untuk membuat object dari Map
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      price: map['price'] as int,
+      image: map['image'] as String,
+    );
+  }
+}
+
+// Model tambahan untuk CartItem
+class CartItemModel {
+  final ProductModel product;
+  int quantity;
+
+  CartItemModel({
+    required this.product,
+    this.quantity = 1,
+  });
+}
