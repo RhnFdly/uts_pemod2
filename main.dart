b3ac8a6cf77a9cb3dcf559d82fc185e912dfc922
RhnFdly <151982785+RhnFdly@users.noreply.github.com>
2025-11-21 15:40:53 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'blocs/cart_cubit.dart';
+import 'pages/cart_home_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Menggunakan BlocProvider untuk menyediakan CartCubit
+    return BlocProvider(
+      create: (context) => CartCubit(),
+      child: MaterialApp(
+        title: 'UTS Pemrograman Mobile',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        home: const CartHomePage(),
+      ),
+    );
+  }
+}
